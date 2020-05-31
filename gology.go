3ae145196360a7184882268a9a63ebff5f9540a5
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-31 17:50:23 +0530

    Execute command using bash -c

--- gology.go
+++ gology.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := "goodluck | goatsay | goldog"
+	out := exec.Command("bash", "-c", cmd)
+	out.Stdout = os.Stdout
+	out.Stderr = os.Stderr
+	err := out.Run()
+	if err != nil {
+		log.Fatalf("cmd.Run failed with %s", err)
+	}
+}
